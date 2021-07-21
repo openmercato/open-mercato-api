93b45f903c4ac86934b0d7159dd30110ec57b94d
Piotr Karwatka <piotr.karwatka@gmail.com>
2021-07-21 09:03:54 +0200

    [chore] admin and marketplace-starter apps removed as this becomes a core API project only

--- apps/admin/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AdminModule } from './admin.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AdminModule);
-  await app.listen(3000);
-}
-bootstrap();
--- apps/marketplace-starter/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { MarketplaceStarterModule } from './marketplace-starter.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(MarketplaceStarterModule);
-  await app.listen(3000);
-}
-bootstrap();
